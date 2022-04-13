234c0a2ffe00d66bd663e436d75e20993c37a8f0
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-13 09:29:43 -0400

    commit tree b4c068691de101bbe75bcc75b59a634a20713246

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+const findMatching = (drivers, string) => {
+    let caseMatch = drivers.filter(
+        (name) => {
+            if (name.toLowerCase() === string.toLowerCase()) {
+                return name;
+            }
+        }
+    )
+    if (caseMatch === undefined) {
+        return [];
+    } else {
+        return caseMatch;
+    }
+}
+
+const fuzzyMatch = (drivers, string) => {
+    let startMatch = drivers.filter(
+            (name) => {
+                let nameArray = Array.from(name);
+                let stringArray = Array.from(string);
+                if (nameArray[0][1] === stringArray[0][1]) {
+                    return name;
+                }
+            }
+        )
+    if (startMatch === undefined) {
+        return [];
+    } else {
+        return startMatch;
+    }
+}
+
